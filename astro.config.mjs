e362ee33380a00053bf76dbe316d774e4e2771bf
zsga <github@gaoa.cc>
2024-04-15 20:54:16 +0800

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,43 +1,35 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
-import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
-import remarkUnwrapImages from 'remark-unwrap-images'
-import rehypeExternalLinks from 'rehype-external-links'
-import expressiveCode from 'astro-expressive-code'
-import { expressiveCodeOptions } from './src/site.config'
-import icon from 'astro-icon'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import { remarkReadingTime } from './src/utils/remarkReadingTime.ts';
+import remarkUnwrapImages from 'remark-unwrap-images';
+import rehypeExternalLinks from 'rehype-external-links';
+import expressiveCode from 'astro-expressive-code';
+import { expressiveCodeOptions } from './src/site.config';
+import icon from 'astro-icon';
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://gaoa.cc',
-	integrations: [
-		expressiveCode(expressiveCodeOptions),
-		tailwind({
-			applyBaseStyles: false
-		}),
-		sitemap(),
-		mdx(),
-		icon()
-	],
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		rehypePlugins: [
-			[
-				rehypeExternalLinks,
-				{
-					target: '_blank',
-					rel: ['nofollow, noopener, noreferrer']
-				}
-			]
-		],
-		remarkRehype: {
-			footnoteLabelProperties: {
-				className: ['']
-			}
-		}
-	},
-	prefetch: true,
-	output: 'server',
-})
+  site: 'https://gaoa.cc',
+  integrations: [expressiveCode(expressiveCodeOptions), tailwind({
+    applyBaseStyles: false
+  }), sitemap(), mdx(), icon()],
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: '_blank',
+      rel: ['nofollow, noopener, noreferrer']
+    }]],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: ['']
+      }
+    }
+  },
+  prefetch: true,
+  output: 'server',
+  adapter: cloudflare()
+});
